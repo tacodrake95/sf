aeb20e8a7454ed0e7b1087ef36fc0067175c8909
jake drake <tacodrake95@yahoo.com>
2020-08-01 17:04:32 -0700

    masjdjdsfdsf

--- soundFilters.py
+++ soundFilters.py
@@ -1,6 +1,43 @@
 import tkinter as tk
 import random
 import sounddevice as sd
+from colour import Color
+
+
+def lerp(lo, hi, pc):
+    return lo + (hi - lo) * pc
+
+def vecLerp(lo, hi, pc):
+    return (lerp(lo[0], hi[0], pc), lerp(lo[1], hi[1], pc), lerp(lo[2], hi[2], pc))
+
+black = Color("#000000")
+red = Color("#FF0000")
+green = Color("#00FF00")
+blue = Color("#0000FF")
+white = Color("#FFFFFF")
+cRange0 = list(red.range_to(blue, 256))
+cRange1 = list(blue.range_to(green, 256))
+cRange2 = list(blue.range_to(white, 256))
+
+def colorCalc(value):
+    value = int((value[0]+0x7FFFFFFF))
+    #print(value)
+    if value < (1<<8):
+        return black
+    
+    elif value < (1<<8):
+        value = value >> 8
+        return cRange0[value]
+    
+    elif value < (1<<16):
+        value = value >> 16
+        return cRange1[value]
+        
+    else:
+        value = value >> 24
+        return cRange2[value]
+                       
+first = 1
 
 class App:
     def __init__(self, t):
@@ -11,34 +48,32 @@ class App:
         
     def refresh(self, i):
         self.panel.configure(image=i)
-        self.panel.image=i
-        
-        
-        
+        self.panel.image=i        
         
 
 
 t = tk.Tk()
 a = App(t)
 def task(t, app):
-    data = sd.rec(4096, channels=1, dtype='int16')
+    data = sd.rec(1024, channels=1, dtype='int32')
     
-    img = tk.PhotoImage(width=64,height=64)
+    img = tk.PhotoImage(width=32,height=32)
     row = 0
     col = 0
     for value in data:
-        value = int(value[0] << 7) & 0xFFFFFF
+        color = colorCalc(value)
+        value = int(value[0] >> 7) & 0xFFFFFF
         value = "#" + hex(value)[2:].zfill(6)
         #print(value)
         
-        img.put(value,(row,col))
+        img.put(color,(row,col))
         col += 1
-        if col == 64:
+        if col == 32:
             row +=1; col =0
 
     img = img.zoom(3)
     app.refresh(img)
-    t.after(100, task, t, app)
+    t.after(25, task, t, app)
 sd.default.samplerate = 40960
 task(t, a)
 t.mainloop()
